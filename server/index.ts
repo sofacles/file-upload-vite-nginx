a7762fa4ba200422aa23c5b0cd656bd3b1e238e3
Steve Degermark <sofacles@yahoo.com>
2023-11-14 18:57:29 -0800

    putting a full url on the image so it goes all the way back to the server, skipping the public folder vite provides

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,5 @@
 import express, { Express, Request, Response } from "express";
-import router from "./apiRouter";
+import multer from "multer";
 import bodyParser from "body-parser";
 import Logger from "./logger";
 
@@ -13,12 +13,49 @@ app.get("/", (_: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
-app.use("/api", router);
-app.use(function (err, req, res, next) {
-  Logger.error(err.stack);
-  res.status(500).send("Something broke!");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, `${process.cwd()}/sandbox/resized`);
+  },
+  filename: function (req, file, cb) {
+    let imageName = req.body.imageName;
+    if (file.mimetype === "image/jpeg") {
+      imageName += ".jpg";
+    } else if (file.mimetype === "image/png") {
+      imageName += ".png";
+    }
+    cb(null, imageName);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  cb(null, true);
+};
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 16,
+  },
+  fileFilter: fileFilter,
+});
+
+export type fileRequest = { file: { filename: string } };
+
+app.post("/api", upload.single("uploadPhoto"), async (req, res) => {
+  if (req.file) {
+    Logger.info(
+      `At ${new Date().toLocaleString()}, we got a chunk of a file for: ${
+        req.file.filename
+      }`
+    );
+  } else {
+    Logger.info("req.file is undefined");
+  }
+
+  res.send("ok");
 });
 
 app.listen(8888, () => {
-  console.log(" web server is running on http://localhost:8888");
+  console.log("web server is running on http://localhost:8888");
 });
